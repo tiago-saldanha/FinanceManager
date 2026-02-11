d08a42e14608ac1a348806ec9e435449ae7339f0
Tiago <Tiago@XEON-2667V4>
2026-02-10 23:07:54 -0300

    remove comment

--- src/FinanceManager.Application/Extensions/ApplicationDependencyInjection.cs
+++ src/FinanceManager.Application/Extensions/ApplicationDependencyInjection.cs
@@ -15,16 +15,3 @@ namespace FinanceManager.Application.Extensions
         }
     }
 }
-/*
- * 
- *          builder.Services.AddProblemDetails();
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            var connection = builder.Configuration.GetConnectionString("DefaultConnection");
-            builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(connection, b => b.MigrationsAssembly("FinanceManager.Infrastructure")));
-            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-            builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
-            builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-            builder.Services.AddScoped<ITransactionAppService, TransactionAppService>();
-            builder.Services.AddScoped<ICategoryAppService, CategoryAppService>();
- */
